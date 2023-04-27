0b04d6dfd57ef8e74ba0576fcfe2a8348c6ded5d
unknown <kyle.hoeckman@gmail.com>
2023-04-28 01:33:45 +0200

    het voorbeeldspel uit figma nagemaakt in css grid (nog niet finaal)

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 "use strict";
 
 (function() {
-    //
+    let board = document.getElementById('board');
+    let grid = document.getElementById('grid');
+    let tiles = document.getElementById('tiles');
+
+    tiles.addEventListener('mouseover', e => {
+        e.stopPropagation();
+    });
+
+    grid.addEventListener('mouseover', e => {
+        console.log(e.target);
+    });
 })();
