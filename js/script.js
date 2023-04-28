2e36bff333b8b322905a9c8a05d13e184567d1f3
unknown <kyle.hoeckman@gmail.com>
2023-04-28 23:10:22 +0200

    check if levels are correctly solved + js and css bugfixes

--- js/script.js
+++ js/script.js
@@ -4,41 +4,55 @@
     const grid = document.getElementById('grid');
     const tiles = document.getElementById('tiles');
 
+    const challenges = {
+        1: {tiles: 'f', gate: 'X', solution: ''},
+        2: {tiles: 'ae', gate: 'T', solution: '[[,,{"n":"e","r":3},],[,,,{"n":"a","r":0}],[,,,],[,,,],[,,,]]', gate: 'T'},
+        63: {tiles: 'bdefghij', gate: 'W', solution: '[[,,,],[,,,],[,,,{"n":"b","r":0}],[,,{"n":"b","r":0},],[,,,]]'}
+    };
+
     class HorseAcademy {
+
         constructor(rows, cols) {
             this.rows = rows;
             this.cols = cols;
-            this.newBoard();
-
-            // test code
-            this.board[3][2] = {name: 'b', rotation: 0};
-            this.renderBoard();
+            this.newBoard(1);
         }
         
-        newBoard() {
+        newBoard(challenge) {
+            // create 2d array
             this.board = [...Array(this.cols)].fill()
                 .map(col => [...Array(this.rows)].fill()
                 .map(cell => null));
+            
+            this.solution = challenges[challenge].solution;
+
+            // test code
+            this.board[0][2] = {name: 'e', rotation: 3};
+            this.board[1][3] = {name: 'a', rotation: 0};
+            // test code end
+
+            this.renderBoard();
         }
 
         renderBoard() {
+            // clear all tiles
             tiles.innerHTML = '';
 
+            // convert all tiles to html elements and put them on the board
             for (let col = 0; col < this.cols; col++) {
                 for (let row = 0; row < this.rows; row++) {
                     const tile = this.board[col][row];
 
                     if (!tile) continue;
 
-                    console.log(tile);
-
                     const img = document.createElement('img');
-                    img.src = `./img/tiles/${tile.name}.png`;
+                    img.src = `img/tiles/${tile.name}.png`;
                     img.alt = 'tile ' + tile.name;
 
                     const div = document.createElement('div');
                     div.appendChild(img);
 
+                    // use css classes to position the tile on the grid
                     div.classList.add(
                         'tile',
                         'tile-' + tile.name,
@@ -51,7 +65,16 @@
                 }
             }
         }
+
+        solved() {
+            return JSON.stringify(this.board)
+                .replaceAll('null', '')
+                .replaceAll('name', 'n')
+                .replaceAll('rotation', 'r') === this.solution;
+        }
     }
 
     const game = new HorseAcademy(4, 5);
+    game.newBoard(2);
+    console.log(game.solved()); // true
 })();
