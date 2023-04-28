5967ba1f5505507b8c73032b803476e142a2c97d
unknown <kyle.hoeckman@gmail.com>
2023-04-28 23:29:19 +0200

    Ik ga slapen slaapwel (js structuur fixes)

--- js/script.js
+++ js/script.js
@@ -4,28 +4,41 @@
     const grid = document.getElementById('grid');
     const tiles = document.getElementById('tiles');
 
+    // all levels
     const challenges = {
-        1: {tiles: 'f', gate: 'X', solution: ''},
-        2: {tiles: 'ae', gate: 'T', solution: '[[,,{"n":"e","r":3},],[,,,{"n":"a","r":0}],[,,,],[,,,],[,,,]]', gate: 'T'},
-        63: {tiles: 'bdefghij', gate: 'W', solution: '[[,,,],[,,,],[,,,{"n":"b","r":0}],[,,{"n":"b","r":0},],[,,,]]'}
+        1: {tiles: 'f', gate: 'x', solution: ''},
+        2: {tiles: 'ae', gate: 't', solution: '[[,,{"n":"e","r":3},],[,,,{"n":"a","r":0}],[,,,],[,,,],[,,,]]'},
+        63: {tiles: 'bdefghij', gate: 'w', solution: '[[,,,],[,,,],[,,,{"n":"b","r":0}],[,,{"n":"b","r":0},],[,,,]]'}
     };
 
     class HorseAcademy {
-
         constructor(rows, cols) {
             this.rows = rows;
             this.cols = cols;
-            this.newBoard(1);
+            this.newChallenge(challenges[1]);
         }
         
-        newBoard(challenge) {
+        newChallenge(challenge) {
+            this.solution = challenge.solution;
+
+            // reset all gates
+            document.querySelectorAll('#grid span').forEach(el => {
+                el.style.backgroundColor = '';
+            });
+
+            // set gate to red
+            document.querySelector('#grid #gate-' + challenge.gate)
+                .style.backgroundColor = 'red';
+
+            this.resetBoard();
+        }
+
+        resetBoard() {
             // create 2d array
             this.board = [...Array(this.cols)].fill()
                 .map(col => [...Array(this.rows)].fill()
                 .map(cell => null));
             
-            this.solution = challenges[challenge].solution;
-
             // test code
             this.board[0][2] = {name: 'e', rotation: 3};
             this.board[1][3] = {name: 'a', rotation: 0};
@@ -75,6 +88,6 @@
     }
 
     const game = new HorseAcademy(4, 5);
-    game.newBoard(2);
+    game.newChallenge(challenges[2]);
     console.log(game.solved()); // true
 })();
