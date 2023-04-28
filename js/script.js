1b6fb6f3e95c0e66d98661804c6fd949b31ae900
unknown <kyle.hoeckman@gmail.com>
2023-04-28 22:21:47 +0200

    the grid can be updated dynamically with a new js class

--- js/script.js
+++ js/script.js
@@ -1,11 +1,57 @@
-"use strict";
+'use strict';
 
 (function() {
-    let board = document.getElementById('board');
-    let grid = document.getElementById('grid');
-    let tiles = document.getElementById('tiles');
+    const grid = document.getElementById('grid');
+    const tiles = document.getElementById('tiles');
 
-    tiles.addEventListener('mouseover', e => {
-        //console.log(e.target);
-    });
+    class HorseAcademy {
+        constructor(rows, cols) {
+            this.rows = rows;
+            this.cols = cols;
+            this.newBoard();
+
+            // test code
+            this.board[3][2] = {name: 'b', rotation: 0};
+            this.renderBoard();
+        }
+        
+        newBoard() {
+            this.board = [...Array(this.cols)].fill()
+                .map(col => [...Array(this.rows)].fill()
+                .map(cell => null));
+        }
+
+        renderBoard() {
+            tiles.innerHTML = '';
+
+            for (let col = 0; col < this.cols; col++) {
+                for (let row = 0; row < this.rows; row++) {
+                    const tile = this.board[col][row];
+
+                    if (!tile) continue;
+
+                    console.log(tile);
+
+                    const img = document.createElement('img');
+                    img.src = `./img/tiles/${tile.name}.png`;
+                    img.alt = 'tile ' + tile.name;
+
+                    const div = document.createElement('div');
+                    div.appendChild(img);
+
+                    div.classList.add(
+                        'tile',
+                        'tile-' + tile.name,
+                        'rotation-' + tile.rotation,
+                        'x-' + row,
+                        'y-' + col
+                    );
+
+                    tiles.appendChild(div);
+                }
+            }
+        }
+    }
+
+    const game = new HorseAcademy(4, 5);
 })();
