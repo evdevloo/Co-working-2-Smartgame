0fdd15dbee47d28a7c34783973bfad5c3f77b90b
Khoeckman <kyle.hoeckman@gmail.com>
2023-05-07 15:43:23 +0200

    board css fixed + javascript buttons for levels changes added

--- js/script.js
+++ js/script.js
@@ -1,21 +1,42 @@
+// Staat in commentaar zodat alles in console gedebugged kan worden - Kyle
+
 //'use strict';
 
 //function() {
     const grid = document.getElementById('grid');
     const tiles = document.getElementById('tiles');
 
-    // all levels
+    const previousChallenge = function(element) {
+        game.newChallenge(challenges[Math.max(0, selectedChallenge - 1)]);
+
+        if (selectedChallenge <= 1) element.style.display = 'none';
+    }
+    
+    const nextChallange = function() {
+        game.newChallenge(challenges[Math.min(64, selectedChallenge + 1)]);
+
+        if (selectedChallenge <= 1) element.style.display = 'none';
+    }
+
+    document.getElementById('previousChallenge')
+        .addListener('click', previousChallenge);
+    document.getElementById('nextChallenge')
+        .addListener('click', nextChallange);
+
+    // all challenges
     const challenges = {
         1: {tiles: 'f', gate: 'x', solution: ''},
         2: {tiles: 'ae', gate: 't', solution: '9844f81e1408f6ec'},
         63: {tiles: 'bdefghij', gate: 'w', solution: ''}
-    };
+        80: {tiles: 'abcdefhij', gate: 't', solution: ''}
+    }
+    const selectedChallenge = 1;
 
     class HorseAcademy {
         constructor(rows, cols) {
             this.rows = rows;
             this.cols = cols;
-            this.newChallenge(challenges[1]);
+            this.newChallenge(challenges[selectedChallenge]);
         }
         
         newChallenge(challenge) {
@@ -84,6 +105,6 @@
     }
 
     const game = new HorseAcademy(4, 5);
-    game.newChallenge(challenges[2]);
+
     console.log(game.solved()); // true
 //})();
