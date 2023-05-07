c30d82631264c187e6974614a5b118e0652cff04
Evert Devloo <evert.devloo@student.odisee.be>
2023-05-07 22:28:42 +0200

    trying to add dragability to the tiles in the slider

--- js/itemBar.js
+++ js/itemBar.js
@@ -1,4 +1,24 @@
 'use strict';
 (function(){
 
+    let dragged;
+
+    const source = document.querySelector('.draggable');
+
+    source.addEventListener("drag", (event) => {
+        console.log("dragging");
+    });
+
+    source.addEventListener("dragstart", (event) => {
+        // store a ref. on the dragged elem
+        dragged = event.target;
+        // make it half transparent
+        event.target.classList.add("dragging");
+    });
+
+    source.addEventListener("dragend", (event) => {
+        // reset the transparency
+        event.target.classList.remove("dragging");
+    });
+
 })();
