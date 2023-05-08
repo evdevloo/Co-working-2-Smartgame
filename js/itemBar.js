1648c07611bf42f87ce8d0af2e0b2ba92b1d70c3
Evert Devloo <evert.devloo@student.odisee.be>
2023-05-08 17:37:13 +0200

    added drag function to tiles

--- js/itemBar.js
+++ js/itemBar.js
@@ -3,22 +3,61 @@
 
     let dragged;
 
-    const source = document.querySelector('.draggable');
+    const tiles = document.getElementsByClassName('draggable');
+    const target = document.getElementById("droptarget");
 
-    source.addEventListener("drag", (event) => {
-        console.log("dragging");
+    for (let i = 0; i < tiles.length; i++) {
+        const tile = tiles[i];
+
+        // plaats een eventListener op de knop
+        tile.addEventListener("drag", () => {
+            console.log("dragging");
+        });
+
+        tile.addEventListener("dragstart", (event) => {
+            // store a ref. on the dragged elem
+            dragged = event.target;
+            // make it half transparent
+            event.target.classList.add("dragging");
+        });
+
+        tile.addEventListener("dragend", (event) => {
+            // reset the transparency
+            event.target.classList.remove("dragging");
+        });
+    }
+
+
+    target.addEventListener(
+        "dragover",
+        (event) => {
+            // prevent default to allow drop
+            event.preventDefault();
+        },
+        false
+    );
+
+    target.addEventListener("dragenter", (event) => {
+        // highlight potential drop target when the draggable element enters it
+        if (event.target.classList.contains("dropzone")) {
+            event.target.classList.add("dragover");
+        }
     });
 
-    source.addEventListener("dragstart", (event) => {
-        // store a ref. on the dragged elem
-        dragged = event.target;
-        // make it half transparent
-        event.target.classList.add("dragging");
+    target.addEventListener("dragleave", (event) => {
+        // reset background of potential drop target when the draggable element leaves it
+        if (event.target.classList.contains("dropzone")) {
+            event.target.classList.remove("dragover");
+        }
     });
 
-    source.addEventListener("dragend", (event) => {
-        // reset the transparency
-        event.target.classList.remove("dragging");
+    target.addEventListener("drop", (event) => {
+        // prevent default action (open as link for some elements)
+        event.preventDefault();
+        // move dragged element to the selected drop target
+        if (event.target.classList.contains("dropzone")) {
+            event.target.classList.remove("dragover");
+            event.target.appendChild(dragged);
+        }
     });
-
 })();
