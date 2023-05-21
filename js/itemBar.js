64f3d3708553e289e2160d06d3067f4fde2394aa
Kyle Hoeckman <kyle.hoeckman@gmail.com>
2023-05-21 18:18:42 +0200

    css fixed + rotation pivot fix

--- js/itemBar.js
+++ js/itemBar.js
@@ -3,8 +3,7 @@ import { game } from './board.js';
 let pieces = null;
 let currentDroppable = null;
 let rotation = 0;
-let deg = 0;
-let tiles_challenge;
+//let tiles_challenge;
 
 getPieces();
 deleteDuplicates();
@@ -12,7 +11,7 @@ deleteDuplicates();
 function getPieces() {
     pieces = document.querySelectorAll(`.card, div.tile `);
 
-    pieces .forEach(piece =>{
+    pieces.forEach(piece => {
         piece.addEventListener('mousedown', onmousedown);
 
         piece.ondragstart = false;
@@ -47,14 +46,14 @@ function onmousedown(event) {
 
     function moveAt(pageX, pageY) {
         piece.style.left = pageX - 50 + 'px';
-        piece.style.top = pageY  - 50 + 'px';
+        piece.style.top = pageY - 50 + 'px';
     }
 
     function onMouseMove(event) {
         moveAt(event.pageX, event.pageY);
     }
 
-    function onMouseUp(event){
+    function onMouseUp(event) {
 
         document.removeEventListener('mousemove', onMouseMove);
         document.removeEventListener('keypress', rotating)
@@ -71,27 +70,27 @@ function onmousedown(event) {
 
         piece.style = '';
         piece.classList.remove('dragging')
-        if ( currentDroppable === null|| (!currentDroppable.classList.contains('droppable')) || currentDroppable.id === 'items') {
+
+        if (currentDroppable === null || (!currentDroppable.classList.contains('droppable')) || currentDroppable.id === 'items') {
             document.querySelector('#items').appendChild(piece);
-        } else if(currentDroppable.parentElement.id === 'grid') {
-            let x =+ currentDroppable.classList[1].slice(-1);
-            let y =+ currentDroppable.classList[2].slice(-1);
-            if(game.addPiece(piece.id,x,y,(rotation%4)) === undefined ){
-                event.target.remove();
-            } else {
-                document.querySelector('#items').appendChild(piece);
-            }
+
+        } else if (currentDroppable.parentElement.id === 'grid') {
+            let x = +currentDroppable.classList[1].slice(-1);
+            let y = +currentDroppable.classList[2].slice(-1);
+
+            if (game.addPiece(piece.id, x, y, rotation % 4) === undefined) piece.remove();
+            else document.querySelector('#items').appendChild(piece);
         }
-        resetSlider(tiles_challenge);
+
+        //resetSlider(tiles_challenge);
         rotation = 0;
-        deg = 0;
         getPieces();
         document.removeEventListener('mouseup', onMouseUp);
     }
 
 }
 
-function rotating(event){
+function rotating(event) {
     if (event.key === 'r') {
         ++rotation;
         document.querySelector('.dragging').style.transform = `rotate(${rotation * 90}deg)`;
@@ -102,18 +101,18 @@ function deleteDuplicates() {
     let copies = Object.values(pieces).filter(piece => piece.classList.contains('tile'));
 
     copies.forEach(piece => {
-        let id = piece.firstChild.alt.charAt(5);
+        let id = piece.firstChild.alt.slice(-1);
         document.querySelector(`#${id}`).remove();
     })
 }
 
 export function resetSlider(tiles) {
-    tiles_challenge = tiles;
+    //tiles_challenge = tiles;
 
     let slider = document.querySelector('#items');
     slider.innerHTML = '';
 
-    [...tiles_challenge].forEach(tile => {
+    [...tiles].forEach(tile => {
         const div = document.createElement('div');
         div.id = tile;
         div.className = 'card';
