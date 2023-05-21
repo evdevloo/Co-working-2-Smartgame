53baaf19ff1b26079802fc0d823ebe9e4dc76502
Izzet <izzet9719@gmail.com>
2023-05-21 23:59:38 +0200

    fixing errors in js file

--- js/popup.js
+++ js/popup.js
@@ -4,17 +4,15 @@
     const openBtn = document.getElementById('open-btn');
     const closeBtn = document.getElementById('close-btn');
     const popup = document.getElementById('popup');
+    const board = document.getElementById('board');
 
     openBtn.addEventListener('click', function () {
         popup.style.display = 'flex';
+        board.style.opacity = 0;
     });
 
     closeBtn.addEventListener('click', function () {
         popup.style.display = 'none';
+        board.style.opacity = 1;
     });
-
-
-
 })();
-
-
