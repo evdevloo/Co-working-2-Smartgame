5acbeec05942d96f99ebd6b8accc372ad742e22b
Izzet <izzet9719@gmail.com>
2023-05-22 18:26:39 +0200

    fixing errors in js and css

--- js/popup.js
+++ js/popup.js
@@ -5,14 +5,20 @@
     const closeBtn = document.getElementById('close-btn');
     const popup = document.getElementById('popup');
     const board = document.getElementById('board');
+    const heading = document.getElementById('challenge-heading');
+    const headingdesc = document.getElementById('challenge-description');
 
     openBtn.addEventListener('click', function () {
         popup.style.display = 'flex';
         board.style.opacity = 0;
+        heading.style.opacity = 0;
+        headingdesc.style.opacity = 0;
     });
 
     closeBtn.addEventListener('click', function () {
         popup.style.display = 'none';
         board.style.opacity = 1;
+        heading.style.opacity = 1;
+        headingdesc.style.opacity = 1;
     });
 })();
